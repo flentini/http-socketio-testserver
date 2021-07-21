1cfadbe35090539c1b2a28355527816a14e8bf56
Francesco Lentini <francesco.lentini@gmail.com>
2021-07-21 17:41:59 +0100

    add socketio

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const app = require('./http');
+const socketio = require('./socketio');
 
 const { log } = console;
 
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
+
+socketio(server);
 
 server.listen(PORT, () => {
   log(`Server listening on port ${PORT}`);
